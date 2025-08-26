e861d21a58b8e623e34622e3d16b8daef28be1f7
andrefg5 <167633809+andrefg5@users.noreply.github.com>
2025-08-26 18:02:13 -0300

    Update verificapf.h

--- verificapf.h
+++ verificapf.h
@@ -18,13 +18,14 @@ bool verifica(char cpf[12])
         bool tigual = true;
         for(int i = 0; i < 11; i++)
         {
-                if(n[i] != n[0]) //verifica todos com o primeiro elemento
+                if(n[i] != n[0]){ //verifica todos com o primeiro elemento
                         tigual = false;
                         break;
+                }
         }
 
         if(tigual) return 1;//finaliza se todos forem iguais
-
+        
         int soma1 = n[0] * 10 + n[1] * 9 + n[2] * 8 + n[3] * 7 + n[4] * 6 + n[5] * 5 + n[6] * 4 + n[7] * 3 + n[8] * 2;
         int resto1 = (soma1 *10) % 11;
 
@@ -34,5 +35,6 @@ bool verifica(char cpf[12])
 
         if(resto2 == 10) resto2 = 0;
 
-        return (resto1 == n[9] && resto2 == n[10]);
+        if(resto1 != n[9]) return false;
+        if(resto2 != n[10]) return false;
 }
